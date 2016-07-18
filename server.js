338efa3a441c14a304c816e85ef384a75b84ccd6
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-07-18 14:57:27 +0200

    add basic files

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('client'));
+
+app.listen(3000);
