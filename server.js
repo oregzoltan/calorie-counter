6b3c668bb81bf40f712f2236ae3eb58196cc7ac5
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-07-18 19:29:05 +0200

    put done

--- server.js
+++ server.js
@@ -5,5 +5,32 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('client'));
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  database: "caloriecounter",
+  host: "localhost",
+  user: "root",
+  password: "velox"
+});
+
+con.connect(function(err){
+  if(err){
+    console.log("Error connecting to Db");
+    return;
+  }
+  console.log("Connection established");
+});
+
+app.post('/meals', function(req, res) {
+  con.query('INSERT INTO meals SET ?', { name: req.body.name, calories: req.body.calories, date: req.body.date}, function(err,row){
+    if(err) {
+      console.log(row);
+      return;
+    }
+  res.send({"status": "ok"});
+  });
+});
+
 
 app.listen(3000);
