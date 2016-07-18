be13dfd6001642869bc92c3382afed327759f0d9
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-07-18 20:10:55 +0200

    get and delete done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('client'));
 var mysql = require("mysql");
@@ -25,12 +25,35 @@ con.connect(function(err){
 app.post('/meals', function(req, res) {
   con.query('INSERT INTO meals SET ?', { name: req.body.name, calories: req.body.calories, date: req.body.date}, function(err,row){
     if(err) {
-      console.log(row);
+      console.log(err);
       return;
     }
   res.send({"status": "ok"});
   });
 });
 
+app.get('/meals', function(req, res) {
+  con.query('SELECT * FROM meals;',function(err,rows){
+    if(err) {
+      console.log(err);
+      return;
+    }
+    res.send(rows);
+  });
+});
+
+app.delete('/meals/:id', function(req, res) {
+  con.query('DELETE FROM meals WHERE id = ' + req.params.id, function(err,row){
+    if(err) {
+      console.log(err.toString());
+      return;
+    }
+    if (row.affectedRows === 1) {
+      res.send({"status": "ok"});
+    } else {
+      res.send({"status": "not exists"});
+    }
+  });
+});
 
 app.listen(3000);
